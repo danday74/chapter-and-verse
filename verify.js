4dcd63fc8679bf4017df9404072b611213750c76
Daniel Lewis <daniellewis777@gmail.com>
2018-02-12 02:40:55 +0000

    Enhanced verify checks

--- verify.js
+++ verify.js
@@ -4,22 +4,34 @@ const osis = require('./js/osis')
 const books = require('./js/books1')
 
 let exitCode = 0
+let totalVerseCount = 0
 
 osis.forEach(osis => {
 
   const id = osis.id
   const book = _.find(books, {id})
+  totalVerseCount += book.verseCount
   const chapterCount = book.chapters.length
-  if (chapterCount !== osis.chapters) {
-    if (chapterCount === 0) {
-      console.warn(chalk.yellow('chapter count is zero for ' + id))
+
+  if (chapterCount === 0) {
+    console.warn(chalk.yellow('skipping ' + id))
+  } else {
+
+    if (chapterCount === osis.chapters) {
+      console.log(chalk.green('chapter count is correct for ' + id))
     } else {
       console.error(chalk.red('chapter count is wrong for ' + id))
       exitCode = 1
     }
-  } else {
-    console.log(chalk.green('chapter count is correct for ' + id))
+
   }
 })
+
+if (totalVerseCount === 31102) {
+  console.log(chalk.green('TOTAL VERSE COUNT IS CORRECT'))
+} else {
+  console.error(chalk.red('TOTAL VERSE COUNT IS WRONG'))
+  exitCode = 1
+}
 
 process.exit(exitCode)
