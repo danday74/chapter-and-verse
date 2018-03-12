5a13ce9c62a9be925fb98b33c84a2ffdc08c7cbf
Daniel Lewis <daniellewis777@gmail.com>
2018-03-12 04:44:10 +0000

    support for browser added

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const {camelCase} = require('lodash')
+const {name} = require('./package.json')
+const library = camelCase(name)
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: __dirname,
+    filename: 'browser.js',
+    library
+  }
+}
